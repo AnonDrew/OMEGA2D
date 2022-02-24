515d5ea3eb72987934ffffb9c04f3ca7b929ea0b
AnonDrew <92941746+AnonDrew@users.noreply.github.com>
2022-02-24 14:55:14 -0500

    Delete OMEGA2D/happyCircle 8 directory

--- OMEGA2D/happyCircle 8/src/Dependencies.lua
+++
@@ -1,6 +0,0 @@
-Class = require 'lib/class'
-
-require 'src/constants'
-
-require 'src/Happy'
-require 'src/PipePair'
--- OMEGA2D/happyCircle 8/src/constants.lua
+++
@@ -1,6 +0,0 @@
-love.window.updateMode(1080, 640)
-
-WIN_WIDTH = love.graphics.getWidth()
-WIN_HEIGHT = love.graphics.getHeight()
-
-SPAWN_RATE = 1.4
